2e3e6b4c18ae5d5144c073d27cafaddc44217f5d
cfspfannon <pfannon@coolfiresolutions.com>
2019-06-24 16:36:22 -0500

    Added Prisma API functionality to project

--- src/index.js
+++ src/index.js
@@ -1,36 +1,25 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-let idCount = links.length
+const { prisma } = require('./generated/prisma-client')
 
 // This implements the schema
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links
+        feed: (root, args, context, info) => context.prisma.links()
     },
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link)
-            return link
-        }
-        // TODO Add more CRUD operations
+        post: (root, args, context) => context.prisma.createLink({
+            url: args.url,
+            description: args.description
+        })
     }
 }
 
 // A bundle passed to server to tell it what API operations are accepted and how to resolve them
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: { prisma }
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
