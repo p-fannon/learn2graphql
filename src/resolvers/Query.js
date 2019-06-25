df927da16b2ced00cbb77f784518adf9b40998b6
cfspfannon <pfannon@coolfiresolutions.com>
2019-06-25 15:28:05 -0500

    Implemented a Feed type in the schema to return a count of links in the database

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -12,7 +12,15 @@ async function feed(parent, args, context, info) {
         first: args.first,
         orderBy: args.orderBy
     })
-    return links
+    const count = await context.prisma.linksConnection({
+        where
+    })
+    .aggregate()
+    .count()
+    return {
+        links,
+        count
+    }
 }
 
 module.exports = {
