37a6156f98b2e95a93440d6394e4da514f1c6423
cfspfannon <pfannon@coolfiresolutions.com>
2019-06-25 11:13:17 -0500

    Added authorization workflow, refactored datamodel to utilize one-to-many relationship between Users and Links

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 // This implements the schema
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: (root, args, context, info) => context.prisma.links()
-    },
-    Mutation: {
-        post: (root, args, context) => context.prisma.createLink({
-            url: args.url,
-            description: args.description
-        })
-    }
+    Query,
+    Mutation,
+    User,
+    Link
 }
 
 // A bundle passed to server to tell it what API operations are accepted and how to resolve them
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: { prisma }
+    context: request => {
+        return { 
+            ...request,
+            prisma 
+        }
+    }
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
